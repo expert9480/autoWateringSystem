c5a7f1159eb698c0612abd1ff1dcb914592e1e10
Michael R <98537683+expert9480@users.noreply.github.com>
2024-10-01 19:45:57 -0500

    Add files via upload

---
+++ camera3.py
@@ -0,0 +1,14 @@
+#camera3.py
+
+import time
+from picamera2 import Picamera2
+picam2 = Picamera2()
+config = picam2.create_preview_configuration()
+picam2.configure(config)
+picam2.start()
+# Wait a moment for the camera to adjust
+time.sleep(2)
+# Capture an image
+picam2.capture_file("test_image.jpg")
+print("Image captured as 'test_image.jpg'")
+picam2.stop()
